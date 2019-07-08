17c2ef856d870fc430b5c011c7b1b0aef348b453
Zied NAIFAR <zied.naifar@gmail.com>
2019-07-08 14:10:12 +0100

    full config

---
+++ src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>I'm the BEST hsqdqsdome Component</div>
+      <button onClick={() => console.log("zied !!!!!!!!")}>Press me!!</button>
+    </div>
+  );
+};
+
+export default Home;
